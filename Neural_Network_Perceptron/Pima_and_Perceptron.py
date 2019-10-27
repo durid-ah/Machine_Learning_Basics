9d4c75f4c83330b51cc36efa23bcd48d125fe180
Durid Ahmad <ahmad.durid96@gmail.com>
2019-10-26 22:13:37 -0600

    finished up part one

--- Neural_Network_Perceptron/Pima_and_Perceptron.py
+++ Neural_Network_Perceptron/Pima_and_Perceptron.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 
 columns = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree',
@@ -25,4 +26,20 @@ scaler.fit(train_data_set)
 train_data_set = scaler.transform(train_data_set)
 test_data_set = scaler.transform(test_data_set)
 
+mlp = MLPClassifier()
+mlp.fit(train_data_set, train_target_set)
+predictions = mlp.predict(test_data_set)
 
+print(confusion_matrix(test_target_set,predictions))
+print(classification_report(test_target_set,predictions))
+print("Accuracy:", accuracy_score(test_target_set, predictions))
+
+mlp_tuple = (100,)
+
+for i in range(1000, 2001, 500):
+    for j in range(3):
+        classifier = MLPClassifier(hidden_layer_sizes=mlp_tuple, max_iter=i)
+        classifier.fit(train_data_set, train_target_set)
+        pred = classifier.predict(test_data_set)
+        print("Accuracy:", accuracy_score(test_target_set, pred), i, j)
+        mlp_tuple = mlp_tuple + (100,)
