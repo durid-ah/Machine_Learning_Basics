edfbd0efc354a04962b1ed0372ab70fa7ab21315
Durid Ahmad <ahmad.durid96@gmail.com>
2019-11-29 16:29:51 -0700

    created datasets and classifier

---
+++ Final_Project_Mock_Up/preprocess_dataset.py
@@ -0,0 +1,36 @@
+
+import json
+from pathlib import Path
+import re
+import random
+
+reviews_data = Path("/mnt/c/Users/ahmad/Downloads") / "review.json"
+training_data = Path("fasttext_dataset_training.txt")
+test_data = Path("fasttext_dataset_test.txt")
+
+# What percent of data to save separately as test data
+percent_test_data = 0.10
+
+def strip_formatting(string):
+    string = string.lower()
+    string = re.sub(r"([.!?,'/()])", r" \1 ", string)
+    return string
+
+with reviews_data.open() as input, \
+     training_data.open("w") as train_output, \
+     test_data.open("w") as test_output:
+    i = 0
+    for line in input:
+        review_data = json.loads(line)
+        i += 1
+        print(i)
+        rating = review_data['stars']
+        text = review_data['text'].replace("\n", " ")
+        text = strip_formatting(text)
+
+        fasttext_line = "__label__{} {}".format(rating, text)
+
+        if random.random() <= percent_test_data:
+            test_output.write(fasttext_line + "\n")
+        else:
+            train_output.write(fasttext_line + "\n")
