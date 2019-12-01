ed5c8ee56c4a6228bf66fba14c2bec05243212f0
Durid Ahmad <ahmad.durid96@gmail.com>
2019-11-30 21:50:40 -0700

    added KNN, Decision tree and GaussianNB for the letter_recognition.py script

--- Ensemble_Learning/letter_recognition.py
+++ Ensemble_Learning/letter_recognition.py
@@ -1,7 +1,10 @@
 import pandas as pd
-from sklearn import model_selection
+from sklearn import model_selection, metrics
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import AdaBoostClassifier
 
 categories = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 categories = list(map(lambda x: x.upper(), categories))
@@ -23,3 +26,29 @@ train_data_set, test_data_set, train_target_set, test_target_set = model_selecti
                                                                                                     shuffle=True,
                                                                                                     train_size=0.3)
 
+scaler = StandardScaler()
+scaler.fit(train_data_set)
+
+train_data_set = scaler.transform(train_data_set)
+test_data_set = scaler.transform(test_data_set)
+
+
+# KNN classification
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(train_data_set, train_target_set)
+
+y_pred = classifier.predict(test_data_set)
+print("KNN Accuracy:", metrics.accuracy_score(test_target_set, y_pred))
+
+# Decision Tree
+clf = DecisionTreeClassifier()
+clf = clf.fit(train_data_set, train_target_set)
+predicted = clf.predict(test_data_set)
+print("Decision Tree Accuracy:", metrics.accuracy_score(test_target_set, predicted))
+
+# GaussianNB
+classifier = GaussianNB()
+classifier.fit(train_data_set, train_target_set)
+prediction_result = classifier.predict(test_data_set)
+
+print("GaussianNB Accuracy:", metrics.accuracy_score(test_target_set, prediction_result))
