993c68a5922acc047d0e0775df0da385593a2025
Durid Ahmad <ahmad.durid96@gmail.com>
2019-12-01 17:19:02 -0700

    finished heart.py

--- Ensemble_Learning/heart.py
+++ Ensemble_Learning/heart.py
@@ -28,9 +28,44 @@ scaler.fit(train_data_set)
 train_data_set = scaler.transform(train_data_set)
 test_data_set = scaler.transform(test_data_set)
 
+# SVC classifier
 svc = SVC(probability=True, kernel='linear')
 svc.fit(train_data_set, train_target_set)
 svc_pred = svc.predict(test_data_set)
 
 print("SVC Accuracy:",
       metrics.accuracy_score(test_target_set, svc_pred))
+
+# Decision Tree
+clf = DecisionTreeClassifier()
+clf = clf.fit(train_data_set, train_target_set)
+predicted = clf.predict(test_data_set)
+print("Decision Tree Accuracy:", metrics.accuracy_score(test_target_set, predicted))
+
+# KNN classification
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(train_data_set, train_target_set)
+
+y_pred = classifier.predict(test_data_set)
+print("KNN Accuracy:", metrics.accuracy_score(test_target_set, y_pred))
+
+# AdaBoost
+gnb = GaussianNB()
+abc = AdaBoostClassifier(n_estimators=50,
+                         base_estimator=gnb,
+                         learning_rate=1)
+
+model = abc.fit(train_data_set, train_target_set)
+y_pred = model.predict(test_data_set)
+print("AdaBoost Accuracy:",metrics.accuracy_score(test_target_set, y_pred))
+
+# Bagging
+dtc = DecisionTreeClassifier()
+bag_model = BaggingClassifier(base_estimator=dtc,
+                              n_estimators=100,
+                              bootstrap=True)
+
+bag_model = bag_model.fit(train_data_set, train_target_set)
+bagging_pred = bag_model.predict(test_data_set)
+print("Bagging Accuracy:",
+      metrics.accuracy_score(test_target_set, bagging_pred))
